6c11f50e81329a5ca029906978a31fb00c6d22b2
Rifa9oct <kohinuraktherr@gmail.com>
2023-09-15 15:05:57 +0600

    set remaining & credit hour and total price

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,31 @@ import Courses from './components/Courses/Courses'
 import Header from './components/Header/Header'
 
 function App() {
-  const [addCourseName, setAddCourseName] =useState([]);
+  const [addCourseName, setAddCourseName] = useState([]);
+  const [remainingHour, setRemainingHour] = useState(20);
+  const [totalHour, setTotalHour] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   const handleAddCourseName = (course) =>{
-    const newAddCourseName = [...addCourseName,course];
-    setAddCourseName(newAddCourseName);
+    const isExist = addCourseName.find(item => item.id === course.id);
+    let totalAmount =course.price;
+    let hour = course.credit;
+    if(isExist){
+      return alert('already exist');
+    }
+    else{
+      addCourseName.forEach(item => {
+        totalAmount +=item.price;
+        hour += item.credit;
+      });
+      const totalRemaining = 20 - hour;
+      const newAddCourseName = [...addCourseName,course];
+      setAddCourseName(newAddCourseName);
+      setRemainingHour(totalRemaining);
+      setTotalHour(hour);
+      setTotalPrice(totalAmount);
+    }
+    
   }
 
   return (
@@ -17,7 +37,12 @@ function App() {
       <Header></Header>
       <div className='flex justify-between'>
         <Courses handleAddCourseName={handleAddCourseName}></Courses>
-        <Cart addCourseName={addCourseName}></Cart>
+        <Cart 
+        addCourseName={addCourseName}
+        remainingHour={remainingHour}
+        totalHour={totalHour}
+        totalPrice={totalPrice}
+        ></Cart>
       </div>
       
     </>
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
 
-const Cart = ({addCourseName}) => {
+const Cart = ({addCourseName,remainingHour,totalHour,totalPrice}) => {
     return (
         <div>
             <div className="w-[312px]  bg-white rounded-xl mt-8 p-6">
-            <h1 className="text-[#2F80ED] text-lg font-bold border-b-2 pb-4">Credit Hour Remaining 0 hr</h1>
+            <h1 className="text-[#2F80ED] text-lg font-bold border-b-2 pb-4">Credit Hour Remaining {remainingHour} hr</h1>
             <h1 className="mt-4 font-bold text-[20px]">Course Name</h1>
             <div className='my-5'>
                 {
@@ -13,15 +13,18 @@ const Cart = ({addCourseName}) => {
                     ))
                 }
             </div>
-            <h3 className="font-medium py-4 border-y-2 mt-6 text-[#1c1b1bcc]">Total Credit Hour : 0</h3>
-            <h2 className="font-semibold mt-4 text-[#1c1b1bcc]">Total Price : 0 USD</h2>
+            <h3 className="font-medium py-4 border-y-2 mt-6 text-[#1c1b1bcc]">Total Credit Hour : {totalHour}</h3>
+            <h2 className="font-semibold mt-4 text-[#1c1b1bcc]">Total Price : {totalPrice} USD</h2>
         </div>
         </div>
     );
 };
 
 Cart.propTypes = {
-    addCourseName: PropTypes.array
+    addCourseName: PropTypes.array,
+    remainingHour: PropTypes.number,
+    totalPrice: PropTypes.number,
+    totalHour: PropTypes.number
 };
 
 export default Cart;
