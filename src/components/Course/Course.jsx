b5bcc3db3c707ed9804f012cbadd4fad5c17d1ff
Rifa9oct <kohinuraktherr@gmail.com>
2023-09-19 01:46:44 +0600

    add button hover color

--- src/components/Course/Course.jsx
+++ src/components/Course/Course.jsx
@@ -19,10 +19,11 @@ const Course = ({course,handleAddCourseName}) => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                     </svg>
+                  
                     <h2 className='text-[#1c1b1b99] font-medium'>Credit : {credit}hr</h2>
                 </div>
             </div>
-            <button onClick={() => handleAddCourseName(course)} className='bg-[#2F80ED] mt-6 py-2 text-white rounded-lg w-[280px]'>Select</button>
+            <button onClick={() => handleAddCourseName(course)} className='bg-[#2F80ED] hover:bg-blue-900 mt-6 py-2 text-white rounded-lg w-[280px]'>Select</button>
         </div>
     );
 };
